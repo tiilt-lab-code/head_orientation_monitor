1f4e770aa9ca917a1e11101d367ccb726a712a30
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-05-29 22:09:05 -0500

    Adding logging, and front-back

--- main.ts
+++ main.ts
@@ -1,9 +1,26 @@
+function pause_log () {
+    if (calibrated == 1) {
+        pause2 = 1
+        datalogger.log(
+        datalogger.createCV("roll", avg_roll),
+        datalogger.createCV("rvar", roll_var),
+        datalogger.createCV("pitch", avg_pitch),
+        datalogger.createCV("pvar", pitch_var),
+        datalogger.createCV("dtime", down_time),
+        datalogger.createCV("datacount", data_count)
+        )
+        pause2 = 0
+    }
+}
+datalogger.onLogFull(function () {
+    pause2 = 1
+    music.playTone(262, music.beat(BeatFraction.Breve))
+})
 input.onButtonPressed(Button.A, function () {
     if (calibrated == 0) {
+        front = 1
         initial_pitch = input.rotation(Rotation.Pitch)
         initial_roll = input.rotation(Rotation.Roll)
-        basic.showNumber(initial_pitch)
-        basic.showNumber(initial_roll)
         reset_vars()
     }
 })
@@ -17,7 +34,6 @@ function reset_vars () {
     calibrated = 1
 }
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(a_b_count)
     if (control.millis() - last_ab >= 5000) {
         a_b_count = 0
         last_ab = control.millis()
@@ -35,6 +51,7 @@ input.onButtonPressed(Button.AB, function () {
         }
         a_b_count = 0
     }
+    pause_log()
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "head_tilt_reset" && team_mode == 1) {
@@ -46,25 +63,20 @@ radio.onReceivedString(function (receivedString) {
         radio.sendValue("dtime", down_time)
         radio.sendValue("iroll", initial_roll)
         radio.sendValue("ipitch", initial_pitch)
+        pause_log()
         reset_vars()
         pause2 = 0
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (calibrated == 1) {
-        pause2 = 1
-        basic.showString("down")
-        basic.showNumber(down_time / data_count)
-        basic.showString("pitch var")
-        basic.showNumber(pitch_var / data_count)
-        basic.showString("roll var")
-        basic.showNumber(roll_var / data_count)
-        basic.showString("avg pitch")
-        basic.showNumber(avg_pitch)
-        basic.showString("avg roll")
-        basic.showNumber(avg_roll)
-        pause2 = 0
+input.onButtonPressed(Button.B, function () {
+    if (calibrated == 0) {
+        initial_pitch = input.rotation(Rotation.Pitch)
+        initial_roll = input.rotation(Rotation.Roll)
+        reset_vars()
     }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    pause_log()
 })
 let pitch_changed = 0
 let roll_changed = 0
@@ -72,9 +84,10 @@ let a_b_count = 0
 let data_count = 0
 let down_time = 0
 let pitch_var = 0
-let roll_var = 0
 let avg_pitch = 0
+let roll_var = 0
 let avg_roll = 0
+let front = 0
 let last_ab = 0
 let team_mode = 0
 let pause2 = 0
@@ -88,17 +101,26 @@ let threshold = 20
 pause2 = 0
 team_mode = 0
 last_ab = control.millis()
-basic.showString("A to calibrate")
-basic.forever(function () {
-    roll_changed = 0
-    pitch_changed = 0
+front = 0
+datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
+basic.showString("A front, B back")
+loops.everyInterval(500, function () {
     if (calibrated == 1 && pause2 == 0) {
+        roll_changed = 0
+        pitch_changed = 0
+        datalogger.log(datalogger.createCV("rot_pot", input.rotation(Rotation.Pitch)), datalogger.createCV("rot_roll", input.rotation(Rotation.Pitch)))
         if (Math.abs(input.rotation(Rotation.Pitch) - initial_pitch) >= threshold) {
             pitch_changed = 1
             pitch_var += 1
             avg_pitch = (data_count * avg_pitch + input.rotation(Rotation.Pitch)) / (data_count + 1)
-            if (input.rotation(Rotation.Pitch) - initial_pitch >= threshold) {
-                down_time += 1
+            if (front == 1) {
+                if (input.rotation(Rotation.Pitch) - initial_pitch >= threshold) {
+                    down_time += 1
+                }
+            } else {
+                if (initial_pitch - input.rotation(Rotation.Pitch) >= threshold) {
+                    down_time += 1
+                }
             }
         }
         if (Math.abs(input.rotation(Rotation.Roll) - initial_roll) >= threshold) {
