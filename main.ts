e58b595c05fec0761dd8fb93ff768c6399d9cfb3
tiilt-lab-code <60939621+tiilt-lab-code@users.noreply.github.com>
2022-06-22 14:56:39 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,18 @@ function pause_log () {
         datalogger.createCV("pitch", avg_pitch),
         datalogger.createCV("pvar", pitch_var / data_count),
         datalogger.createCV("dtime", down_time / data_count),
-        datalogger.createCV("datacount", data_count)
+        datalogger.createCV("datacount", data_count),
+        datalogger.createCV("side_right", roll_right / data_count),
+        datalogger.createCV("side_left", roll_left / data_count)
         )
-        basic.showString("downtime")
+        basic.showString("down")
         basic.showNumber(down_time / data_count)
+        basic.showString("up")
+        basic.showNumber((pitch_var - down_time) / data_count)
+        basic.showString("right")
+        basic.showNumber(roll_right / data_count)
+        basic.showString("left")
+        basic.showNumber(roll_left / data_count)
         pause2 = 0
     }
 }
@@ -19,12 +27,11 @@ datalogger.onLogFull(function () {
     music.playTone(262, music.beat(BeatFraction.Breve))
 })
 input.onButtonPressed(Button.A, function () {
-    if (calibrated == 0) {
-        front = 1
-        initial_pitch = input.rotation(Rotation.Pitch)
-        initial_roll = input.rotation(Rotation.Roll)
-        reset_vars()
-    }
+    front = 1
+    initial_pitch = input.rotation(Rotation.Pitch)
+    initial_roll = input.rotation(Rotation.Roll)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    reset_vars()
 })
 function reset_vars () {
     avg_roll = 0
@@ -33,6 +40,8 @@ function reset_vars () {
     pitch_var = 0
     down_time = 0
     data_count = 0
+    roll_right = 0
+    roll_left = 0
     calibrated = 1
 }
 input.onButtonPressed(Button.AB, function () {
@@ -75,21 +84,26 @@ input.onButtonPressed(Button.B, function () {
     if (calibrated == 0) {
         initial_pitch = input.rotation(Rotation.Pitch)
         initial_roll = input.rotation(Rotation.Roll)
+        music.playTone(988, music.beat(BeatFraction.Double))
         reset_vars()
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     pause_log()
 })
+let c_pitch = 0
+let c_roll = 0
 let pitch_changed = 0
 let roll_changed = 0
 let a_b_count = 0
+let roll_left = 0
 let down_time = 0
 let pitch_var = 0
 let avg_pitch = 0
 let data_count = 0
 let roll_var = 0
 let avg_roll = 0
+let roll_right = 0
 let front = 0
 let last_ab = 0
 let team_mode = 0
@@ -105,34 +119,42 @@ pause2 = 0
 team_mode = 0
 last_ab = control.millis()
 front = 0
+roll_right = 0
 datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
-basic.showString("A front, B back")
+basic.showString("A/B")
 loops.everyInterval(500, function () {
     if (calibrated == 1 && pause2 == 0) {
         roll_changed = 0
         pitch_changed = 0
+        c_roll = input.rotation(Rotation.Roll)
+        c_pitch = input.rotation(Rotation.Pitch)
         datalogger.log(
-        datalogger.createCV("rot_pot", input.rotation(Rotation.Pitch)),
-        datalogger.createCV("rot_roll", input.rotation(Rotation.Roll))
+        datalogger.createCV("rot_pit", c_pitch),
+        datalogger.createCV("rot_roll", c_roll)
         )
-        if (Math.abs(input.rotation(Rotation.Pitch) - initial_pitch) >= threshold) {
+        if (Math.abs(c_pitch - initial_pitch) >= threshold) {
             pitch_changed = 1
             pitch_var += 1
-            avg_pitch = (data_count * avg_pitch + input.rotation(Rotation.Pitch)) / (data_count + 1)
+            avg_pitch = (data_count * avg_pitch + c_pitch) / (data_count + 1)
             if (front == 1) {
-                if (input.rotation(Rotation.Pitch) - initial_pitch >= threshold) {
+                if (c_pitch - initial_pitch >= threshold) {
                     down_time += 1
                 }
             } else {
-                if (initial_pitch - input.rotation(Rotation.Pitch) >= threshold) {
+                if (initial_pitch - c_pitch >= threshold) {
                     down_time += 1
                 }
             }
         }
-        if (Math.abs(input.rotation(Rotation.Roll) - initial_roll) >= threshold) {
+        if (Math.abs(c_roll - initial_roll) >= threshold) {
             roll_changed = 1
             roll_var += 1
-            avg_roll = (data_count * avg_roll + input.rotation(Rotation.Roll)) / (data_count + 1)
+            avg_roll = (data_count * avg_roll + c_roll) / (data_count + 1)
+            if (c_roll > initial_roll) {
+                roll_left += 1
+            } else {
+                roll_right += 1
+            }
         }
         data_count += 1
     }
